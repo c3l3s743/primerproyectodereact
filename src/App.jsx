cf83f28a8b7ff3f8b28c433122b81eb8694f9af6
Melissa <meliss.cele@gmail.com>
2024-05-07 19:23:51 -0300

    Se guardo el nuevo proyecto de react de programacion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+import Contador from './componentes/Contador'
+import './App.css'
+
+export default class App extends Component{
+  render(){
+    return(
+      <div className='ListaContadores'>
+        <Contador> Alumnos </Contador >
+        <Contador> Profesores </Contador>
+        <Contador> Aulas </Contador >
+        <Contador></Contador >
+      </div>
+    )
+  }
+}
---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react'
+import Boton from './Boton'
+
+export default class Contador extends Component{
+  constructor(props){
+    super(props);
+    this.state = { valor: 0}
+  }
+  restar() {
+    this.setState({valor: this.state.valor - 1});
+    // NO => this.state.valor =xxxx;
+  }
+  sumar() {
+    this.setState({valor: this.state.valor + 1});
+  }
+
+  render(){
+    return(
+      <div className="Contador">
+        <span className='Titulo'>{this.props.children}</span>
+        <span className='Valor'>{this.state.valor}</span>
+        <div className="Botonera">
+          <Boton simbolo='-'
+          accion={() => this.restar()}
+          />
+          <Boton simbolo='+'
+          accion={() => this.sumar()}
+          />
+        </div>
+      </div>
+    )
+  }
+}
