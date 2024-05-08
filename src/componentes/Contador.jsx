ebe1fa232e3a2a09415e5e50b5948e6ce2f4f991
Melissa <meliss.cele@gmail.com>
2024-05-08 19:36:51 -0300

    Se agregaron mas botones con funciones

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -13,6 +13,18 @@ export default class Contador extends Component{
   sumar() {
     this.setState({valor: this.state.valor + 1});
   }
+  multiplicar2() {
+    this.setState({valor: this.state.valor * 2});
+  } 
+  multiplicar3() {
+    this.setState({valor: this.state.valor * 3});
+  } 
+  multiplicar4() {
+    this.setState({valor: this.state.valor * 4});
+  }
+  multiplicar5() {
+    this.setState({valor: this.state.valor * 5});
+  }
 
   render(){
     return(
@@ -26,6 +38,18 @@ export default class Contador extends Component{
           <Boton simbolo='+'
           accion={() => this.sumar()}
           />
+          <Boton simbolo='2x'
+         accion={() => this.multiplicar2()}
+         />
+           <Boton simbolo='3x'
+          accion={() => this.multiplicar3()}
+          />
+          <Boton simbolo='4x'
+          accion={() => this.multiplicar4()}
+          />
+          <Boton simbolo='5x'
+         accion={() => this.multiplicar5()}
+         />
         </div>
       </div>
     )
